df01f6e16ce66cbff3cf6afb3b8366ba6182182a
Melvin-48 <melotherock1748@gmail.com>
2026-08-03 09:29:06 +0300

    CORS setup

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,12 @@ function configureDns() {
 async function bootstrap() {
   configureDns();
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
 
